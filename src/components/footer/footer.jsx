1ef16f5b342659169d99865a76eef0120c329b21
olivier-gm <152037529+olivier-gm@users.noreply.github.com>
2025-01-13 09:47:42 -0400

    Update footer.jsx

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -5,24 +5,30 @@ const Footer = () => (
   <footer className="footer">
     <div className="column">
       <h3>Facultades</h3>
-      <a href="#">Departamentos Centrales</a>
+      <a href="/departamentos-centrales" title="Departamentos Centrales">Departamentos Centrales</a>
     </div>
     <div className="column">
       <h3>Pregrado</h3>
-      <a href="#">Información</a>
+      <a href="/informacion-pregrado" title="Información de Pregrado">Información</a>
     </div>
     <div className="column">
       <h3>Webmail</h3>
-      <a href="#">Consulta de Trabajo</a>
+      <a href="/consulta-trabajo" title="Consulta tu trabajo">Consulta de Trabajo</a>
     </div>
     <div className="column">
       <h3>WorldMUN UCV</h3>
-      <a href="#">Estudiantes de Paz</a>
+      <a href="/estudiantes-de-paz" title="Conoce a los estudiantes de paz">Estudiantes de Paz</a>
     </div>
     <div className="social-media">
-    <a href="#"><img height={25} src="fbblue.png" alt="Facebook" /></a>
-      <a href="#"><img height={32} src="xblue.png" alt="Twitter" /></a>
-      <a href="#"><img height={30} src="igblue.png" alt="Instagram" /></a>
+      <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" title="Facebook">
+        <img src="fbblue.png" alt="Facebook logo" />
+      </a>
+      <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" title="Twitter">
+        <img src="xblue.png" alt="Twitter logo" />
+      </a>
+      <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" title="Instagram">
+        <img src="igblue.png" alt="Instagram logo" />
+      </a>
     </div>
   </footer>
 );
