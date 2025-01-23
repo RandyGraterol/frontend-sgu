dae30aa3575c1e149226ee8cabc2a3106095d18f
artur282 <luisarturocruz57@gmail.com>
2025-01-23 17:49:03 -0400

    responsive en tablas grupos de usuarios,crear grupos de usuarios y control de acceso

--- src/screens/controlA.jsx
+++ src/screens/controlA.jsx
@@ -1,67 +1,71 @@
-import React from 'react';
-import './styles/responsiveTable.css';
+import React from "react";
+import "./styles/responsiveTable.css"; // Importa el archivo CSS
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEllipsisH, faUserLock } from "@fortawesome/free-solid-svg-icons";
+import Registro from "../particulas/registro/registro.jsx";
+import Filtro from "../particulas/filtro/filtro.jsx";
 
-// Iconos de Font Awesome
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faEllipsisH,faUserLock} from '@fortawesome/free-solid-svg-icons';
-// Importar partículas
-import Registro from '../particulas/registro/registro.jsx';
-import Filtro from '../particulas/filtro/filtro.jsx';
+const ControlAcceso = ({ icono, name, info }) => {
+  const lista = [];
 
-const ControlAcceso = ({icono,name,info})=>{
-	  const lista = [];
-    
-    // Generar las filas de materias
-    for (let x = 0; x < 10; x++) {
-        lista.push(
-            <tr key={x}>
-                <td>elrandygraterol@gmail.com</td>
-                <td>Admin</td>
-                <td>AIS</td>
-                <td>28/09/24</td>
-                <td>
-                    <div className='botoneraTable'>
-                        <FontAwesomeIcon className='icon' title='Ver' icon={faEllipsisH} size="2x" color="blue" />
-                    </div>
-                </td>
+  // Generar las filas de la tabla
+  for (let x = 0; x < 10; x++) {
+    lista.push(
+      <tr key={x}>
+        <td data-label="Usuario">elrandygraterol@gmail.com</td>
+        <td data-label="Grupo">Admin</td>
+        <td data-label="Sede">AIS</td>
+        <td data-label="Fecha y Hora">28/09/24</td>
+        <td data-label="Acción">
+          <div className="botoneraTable">
+            <FontAwesomeIcon
+              className="icon"
+              title="Ver"
+              icon={faEllipsisH}
+              size="2x"
+              color="blue"
+            />
+          </div>
+        </td>
+      </tr>
+    );
+  }
+
+  return (
+    <>
+      <div className="containerTituloBotones">
+        <center>
+          <h1 className="tituloH1">Control de acceso</h1>
+        </center>
+        <div className="containerBotones">
+          <Filtro textoDefiltro="Sede , grupo de usuario" />
+          <center>
+            <Registro />
+          </center>
+        </div>
+        <div className="titulo">
+          <FontAwesomeIcon icon={icono} size="4x" color="blue" />
+          <h1 className="tituloH1">
+            {name} {info}
+          </h1>
+        </div>
+      </div>
+      <div className="containerTable">
+        <table className="styledTable">
+          <thead>
+            <tr>
+              <th>Usuario</th>
+              <th>Grupo</th>
+              <th>Sede</th>
+              <th>Fecha y Hora</th>
+              <th>Acción</th>
             </tr>
-        );
-    }
-	return(
-		<>
-
-		<div className='containerTituloBotones'>
-
-		<div className='containerBotones'>
-		<Registro />
-		<Filtro textoDefiltro='Sede , grupo de usuario'/>
-		</div>
-
-		<div className='titulo'>
-		<FontAwesomeIcon icon={icono} size="4x" color="blue" />
-		<h1 className='tituloH1'>{name} {info}</h1>
-		</div>
-		</div>
-
-		<div className='containerTable'>
-		<table className='styled-table'>
-		<thead>
-		<tr>
-		<th>Usuario</th>
-		<th>grupo</th>
-		<th>sede</th>
-		<th>fecha y hora</th>
-		<th>Acción</th>
-		</tr>
-		</thead>
-		<tbody>
-		{lista}
-		</tbody>
-		</table>
-		</div>
-
-		</>
-		)
-}
+          </thead>
+          <tbody>{lista}</tbody>
+        </table>
+      </div>
+    </>
+  );
+};
 
 export default ControlAcceso;
--- src/screens/grupoU.jsx
+++ src/screens/grupoU.jsx
@@ -1,70 +1,62 @@
 import React from 'react';
 import './styles/responsiveTable.css';
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSync,faUserLock,faSignInAlt,faEdit,faPlus,faUsers} from '@fortawesome/free-solid-svg-icons';
-
-//importar particulas  registro y filtro
+import { faUsers, faPlus } from '@fortawesome/free-solid-svg-icons';
 import Registro from '../particulas/registro/registro.jsx';
 import Filtro from '../particulas/filtro/filtro.jsx';
 
-
-const GrupoU = ({name,onRegisterClick,onAsignarGU})=>{
-	const usuarios = [];
-for(let x = 0; x < 10 ;x++){
-usuarios.push( 
+const GrupoU = ({ name, onRegisterClick, onAsignarGU }) => {
+    const usuarios = [];
+    for (let x = 0; x < 10; x++) {
+        usuarios.push(
             <tr key={x}>
-            <td>
-            <input type="checkbox" id="miCheckbox" name="miCheckbox" className='checkbox-grande'/>
-            </td>
-            <td>AdminUserpath</td>
-            <td>Calabozo</td>
-            <td>Ingenieria en informatica</td>
-            <td>Activo</td>
-            <td>Super Admin</td>
-            <td>
-              <FontAwesomeIcon onClick={onAsignarGU} title='Asignar Grupo' icon={faPlus} size='2x' color='green' className='iconoAsignarGU'/>
-            </td>
-        </tr>
+                <td data-label="Usuario">AdminUserpath</td>
+                <td data-label="Sede">Calabozo</td>
+                <td data-label="Carrera">Ingenieria en informatica</td>
+                <td data-label="Status">Activo</td>
+                <td data-label="Grupo Usuario">Super Admin</td>
+                <td data-label="Acciones">
+                    <FontAwesomeIcon onClick={onAsignarGU} title='Asignar Grupo' icon={faPlus} size='2x' color='green' className='iconoAsignarGU' />
+                </td>
+            </tr>
         );
-}
-	return(
-		<>
-		 <div className='containerTituloBotones'>
-        <div className='containerBotones'>
-        <Registro onRegisterClick={onRegisterClick}/>
-        <Filtro textoDefiltro='Grupo usuario, sede , carrera'/>
-        </div>
-          
-          <div className='titulo'>
-          <FontAwesomeIcon icon={faUsers} size="4x" color="blue" /> 
-         <h1 className='tituloH1'>{name}</h1> 
+    }
 
-          </div>
-        </div>
-        
-        <div className='containerTable'>
-          <table className='styledTable'>
-         <thead>
-         <tr>
-         <th></th>
-          <th>Usuario</th>
-          <th>Cede</th>
-          <th>Carrera</th>
-          <th>Status</th>
-          <th>Grupo Usuario</th>
-          <th></th>
-         </tr>
-         </thead>
-         <tbody>
-    
-          {usuarios}
-
-         </tbody>
-        
-          </table>
-        </div>
-		</>
-		)
+    return (
+        <>
+            <div className="containerTituloBotones">
+                <div className="titulo">
+                    <FontAwesomeIcon icon={faUsers} size="4x" color="blue" />
+                    <center>
+                        <h1 className="tituloH1">{name}</h1>
+                    </center>
+                </div>
+                <div className="containerBotones">
+                    <Filtro textoDefiltro="Grupo usuario, sede , carrera" />
+                </div>
+                    <center>
+                        <Registro onRegisterClick={onRegisterClick} />
+                    </center>
+            </div>
+            <div className="containerTable">
+                <table className="styledTable">
+                    <thead>
+                        <tr>
+                            <th>Usuario</th>
+                            <th>Sede</th>
+                            <th>Carrera</th>
+                            <th>Status</th>
+                            <th>Grupo Usuario</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {usuarios}
+                    </tbody>
+                </table>
+            </div>
+        </>
+    );
 }
+
 export default GrupoU;
