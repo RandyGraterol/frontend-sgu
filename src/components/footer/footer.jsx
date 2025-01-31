9900743e3b46abae418f988fa2d79c2c98178ee6
olivier-gm <152037529+olivier-gm@users.noreply.github.com>
2025-01-31 07:39:15 -0400

    Add files via upload

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,27 +3,19 @@ import './footer.css';
 
 const Footer = () => (
   <footer className="footer">
+    
     <div className="column">
-      <h3>Facultades</h3>
-    </div>
-    <div className="column">
-      <h3>Pregrado</h3>
-    </div>
-    <div className="column">
-      <h3>Webmail</h3>
-    </div>
-    <div className="column">
-      <h3>WorldMUN UCV</h3>
+      <h3>© Copyright 2025. Sistema De Gestion Universitaria</h3>
     </div>
     <div className="social-media">
       <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" title="Facebook">
-        <img className='fb' src="fbblue.png" alt="Facebook logo" />
+        <img src="fbblue.png" alt="Facebook logo" />
       </a>
       <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" title="Twitter">
-        <img className='tt' src="xblue.png" alt="Twitter logo" />
+        <img src="xblue.png" alt="Twitter logo" />
       </a>
       <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" title="Instagram">
-        <img className='ig' src="igblue.png" alt="Instagram logo" />
+        <img src="igblue.png" alt="Instagram logo" />
       </a>
     </div>
   </footer>
