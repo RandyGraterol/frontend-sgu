9ad419e8384956256620b6be1c3a6275839c1fa5
rjrondon28 <ricardojoserondonuvieda@egmail.com>
2025-01-31 01:45:41 -0400

    Perfil de usario finalizado, En espera de aprobacion.git add .git add .git add .git add .git add .

--- src/components/Form/FormRegistroPerfil/Form.jsx
+++ src/components/Form/FormRegistroPerfil/Form.jsx
@@ -1,73 +1,66 @@
-import { InputBox } from "./ImputBox/InputBox";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Submit from "../../../particulas/SubmitButtons/Submit";
-import Style from "../../../../public/estilosGenerales/formularios.module.css";
+import Style from "/public/estilosGenerales/formularios.module.css";
+import styles from "./form.module.css";
 
 import {
-  faCamera,
   faUser,
+  faPhone,
+  faCamera,
+  faEnvelope,
+  faClipboard,
   faLaptopFile,
   faLaptopCode,
-  faClipboard,
-  faPhone,
+  faLocationDot,
   faMapLocationDot,
-  faEnvelope,
 } from "@fortawesome/free-solid-svg-icons";
 
 export const Form = ({ action, method }) => {
   return (
     <>
-      <section className={Style.ScreenContainer}>
-        <form className={Style.Form} method={method}>
-          <div className={Style.Global}>
-            <div className={Style.Main}>
-              <h3 className={Style.Main_Title}>Información del usuario</h3>
-              <h3 className={Style.Main_Title}>Datos Personales:</h3>
-              <div className={Style.Paragraph_Group}>
-                <p className={Style.Paragraph}>Dirección: </p>
-                <p className={Style.Paragraph_Text}>Castrero</p>
-                <p className={Style.Paragraph}>Fecha de Nacimiento:</p>
-                <p className={Style.Paragraph_Text}>22/03/2025</p>
-              </div>
-              <h3 className={Style.Main_Title}>Otros:</h3>
-              <div className={Style.Paragraph_Group}>
-                <p className={Style.Paragraph}>Género:</p>
-                <p className={Style.Paragraph_Text}>Masculino</p>
-                <p className={Style.Paragraph}>Etnía:</p>
-                <p className={Style.Paragraph_Text}>Genghis Khan</p>
-              </div>
-            </div>
-            <div className={Style.Names}>
-              <div className={Style.Icon}>
-                <FontAwesomeIcon icon={faUser} className={Style.Icon_User} />
-                <FontAwesomeIcon icon={faCamera} className={Style.Icon_Cam} />
-              </div>
-              <h3 className={Style.Names_Title}>Nombre Completo:</h3>
-              <div className={Style.PGroup}>
-                <p className={Style.PGroup_Paragraph}>Tom</p>
-                <p className={Style.PGroup_Paragraph}>Clancys</p>
-                <p className={Style.PGroup_Paragraph}>Rainbow</p>
-                <p className={Style.PGroup_Paragraph}>Six</p>
-              </div>
-              <h3 className={Style.Names_Title}>Contacto:</h3>
-              <div className={Style.PGroup}>
-                <FontAwesomeIcon icon={faPhone} className={Style.Pgroup_Icon} />
-                <p className={Style.PGroup_Paragraph}>04242525994</p>
-              </div>
-              <div className={Style.PGroup}>
-                <FontAwesomeIcon
-                  icon={faEnvelope}
-                  className={Style.Pgroup_Icon}
-                />
-                <p className={Style.PGroup_Paragraph}>pepe@gmail.com</p>
-              </div>
-              <h3 className={Style.Names_Title}>Parroquía:</h3>
-              <div className={Style.PGroup}>
-                <p className={Style.PGroup_Paragraph}>Guárico</p>
-              </div>
-            </div>
+      <section className={styles.contains}>
+        <div className={styles.header}>
+          <div className={styles.boxImg}>
+            <img src="https://www.4x4.ec/overlandecuador/wp-content/uploads/2017/06/default-user-icon-8.jpg" alt=""/>
           </div>
-        </form>
+          <div className={styles.tituloPerfil}>
+            <h1 className={styles.h1}>Rafael Oliveros</h1>
+            <span className={styles.span}>Estudiante - Operador</span>
+          </div>
+        </div>
+        <div className={styles.main}>
+
+          <aside className={styles.aside}>
+
+            <div className={styles.contactos}>
+              <h2 className={styles.h2}>Contactos</h2>
+              <p className={styles.p}><FontAwesomeIcon icon={faPhone} />58- 426-1425358</p>
+              <p className={styles.p}><FontAwesomeIcon icon={faEnvelope } />rafaeloliveros@gmail.com</p>
+              <p className={styles.p}><FontAwesomeIcon icon={faLocationDot} />Pueblo Nuevo/Calle Uruguay</p>
+            </div>
+
+            <div className={styles.informacion}>
+              <h2 className={styles.h2}>Informacion</h2>
+              <p className={styles.p}>Nombre:<span>Rafael Ernesto</span></p>
+              <p className={styles.p}>Apellido:<span>Oliveros Aguilera</span></p>
+              <p className={styles.p}>Edad:<span>39 Años</span></p>
+              <p className={styles.p}>Cedula:<span>28.888.111</span></p>
+              <p className={styles.p}>Nacimiento:<span>28/11/1999</span></p>
+            </div>
+
+          </aside>
+
+          <aside className={styles.aside1}>
+            <div className={styles.informacionPersonal}>
+              <h2 className={styles.h2}>Info. del estudiante</h2>
+              <p className={styles.p}>. Estatus:<span>Actico</span></p>
+              <p className={styles.p}>. Periodo:<span>II</span></p>
+              <p className={styles.p}>. Corte:<span>2014-1</span></p>
+              <p className={styles.p}>. Semestre:<span>"4-5-7"</span></p>
+              <p className={styles.p}>. Ednia:<span>Indio</span></p>
+            </div>
+          </aside>
+        </div>
       </section>
     </>
   );
--- src/components/Form/FormRegistroPerfil/Imput/Input.jsx
+++
@@ -1,7 +0,0 @@
-import Style from "../../../../../public/estilosGenerales/formularios.module.css";
-
-export const Input = ({type,id, placeholder}) =>{
-	return (
-		<input  className={Style.input} type={type} id={id} placeholder={placeholder} required/>
-	);
-}
